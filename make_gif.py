f4f4a06051d1ef5c292d28ff4720438e6793ef4e
Min-Syue <minsyue0504@gmail.com>
2022-09-15 15:02:55 +0800

    Cifar10_50&100Epochs

--- make_gif.py
+++ make_gif.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     
     # file_path = '/photo_cGAN/photo_mnist/Images_Epochs_'\
     file_path = '/photo_cGAN/photo_cifar/Images_Epochs_'
-    make_gif(file_path, 'cGAN_50epochs_cifar', top_range=100)
+    make_gif(file_path, 'cGAN_50epochs_cifar', top_range=50)
